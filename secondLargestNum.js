306f769603d87b2408dbe9302ce37a5f5bba71d7
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-12 12:57:57 +0530

    second largest number

---
+++ secondLargestNum.js
@@ -0,0 +1,17 @@
+class Solution {
+    // Function returns the second largest element
+    getSecondLargest(arr) {
+        arr.sort((a,b)=> a-b);
+        let set = new Set(arr);
+        const newArray = Array.from(set);
+        console.log('newArray...?',newArray);
+
+        if(newArray.length === 1) return -1;
+        return newArray[newArray.length-2];
+    }
+};
+
+const obje = new Solution();
+// console.log(obje.getSecondLargest([12, 35, 1, 10, 34, 1]));
+// console.log(obje.getSecondLargest([10,10]));
+console.log(obje.getSecondLargest([10,5,10]));

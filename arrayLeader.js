e6adbaf251978583b021c3cabead3573dc493ad1
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-12 14:39:17 +0530

    arrayLeader

--- arrayLeader.js
+++ arrayLeader.js
@@ -6,7 +6,7 @@ class Solution {
         const tempArr = [];
         tempArr.push(a[a.length-1]);
         for(let i=a.length-1;i>=0;i--){
-            if(a[i-1] > tempElement){
+            if(a[i-1] >= tempElement){
                 tempElement = a[i-1];
                 tempArr.push(a[i-1]);
             }
@@ -17,4 +17,7 @@ class Solution {
 };
 
 const  ldr = new Solution();
-console.log(ldr.leaders([16, 17, 4, 3, 5, 2]));
+// console.log(ldr.leaders([16, 17, 4, 3, 5, 2]));
+// console.log(ldr.leaders([10, 4, 2, 4, 1]));
+// console.log(ldr.leaders([5, 10, 20, 40]));
+console.log(ldr.leaders([30, 10, 10, 5]));

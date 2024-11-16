4b4a87c1abf3facffa2b90fbc4e28cfa1063c993
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-16 14:05:15 +0530

    frequencyElement

--- firstRepeatedChar.js
+++ firstRepeatedChar.js
@@ -1,16 +1,33 @@
 class Solution {
   firstRepChar(s) {
-    s = s.split('').sort().join('');
-    console.log('s...',s);
-    for(let i=0; i<s.length-1; i++) {
-        if(s.charAt(i) === s.charAt(i+1)){
-            return s.charAt(i);
-        }
+    //first 
+    // s = s.split('').sort().join('');
+    // console.log('s...',s);
+    // for(let i=0; i<s.length-1; i++) {
+    //     if(s.charAt(i) === s.charAt(i+1)){
+    //         return s.charAt(i);
+    //     }
+    // }
+
+    // for(let i=0; i<s.length-1; i++) {
+    //     for(let j=i+1; j<s.length; j++){
+    //         if(s.charAt(i) === s.charAt(j)){
+    //              return s.charAt(i);
+    //         }
+    //     }
+    // }
+
+    let tempArr = [];
+    for(let i=0;i<s.length;i++){
+        tempArr[s.charAt(i)] = (tempArr[s.charAt(i)] || 0) + 1;
     }
     
-    return -1;
+    console.log('tempArr...',tempArr);
+    // return -1;
   }
 };
 
 const rChar = new Solution();
-console.log(rChar.firstRepChar('geeksforgeeks'));
+// console.log(rChar.firstRepChar('geeksforgeeks'));
+// console.log(rChar.firstRepChar('abdc'));
+console.log(rChar.firstRepChar('evjxpnqgmvfjl'));

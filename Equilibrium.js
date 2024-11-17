808c1eff3c7eb0d76f31dd29c901265ff75da3a9
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-17 22:52:03 +0530

    equilibrium

---
+++ Equilibrium.js
@@ -0,0 +1,21 @@
+class Solution {
+    // Function to find equilibrium point in the array.
+    equilibriumPoint(arr) {
+        let leftSum = 0, rightSum = 0;
+        for(let i=0;i<arr.length/2+1;i++){
+            leftSum += arr[i];
+            rightSum += arr[arr.length-i-1];
+            if(leftSum === rightSum){
+                return i+1;
+            }
+        };
+
+        return -1;
+    }
+}
+
+const equilibrium = new Solution();
+
+console.log(equilibrium.equilibriumPoint([1, 3, 5, 2, 2]));
+console.log(equilibrium.equilibriumPoint([1]));
+console.log(equilibrium.equilibriumPoint([1,2,3]));

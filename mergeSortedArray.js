42e0a43f319bf3041f47d9d67c5416a4c3333eee
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-21 12:45:51 +0530

    mergeSortedArray

---
+++ mergeSortedArray.js
@@ -0,0 +1,7 @@
+var merge = function(nums1, m, nums2, n) {
+   nums1.splice(m, nums1.length - m,...nums2);
+   
+   console.log('nums11',nums1);
+};
+
+merge([1,2,3,0,0,0], 3, [2,5,6], 3); // [1,2,2,3,5,6]

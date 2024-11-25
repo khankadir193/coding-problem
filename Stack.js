3ce27c843d9f625a05d01d21cff51192b90038cd
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-25 15:14:20 +0530

    return stackElement

--- Stack.js
+++ Stack.js
@@ -4,14 +4,27 @@ function Stack(arr){
 };
 
 Stack.prototype.push = function (element,stackObj){
+    if(stackObj.arr.length >= 5) return overFlow();
+
     stackObj.arr.push(element);
+    return "Element pushed successfully";
 };
 
 Stack.prototype.pop = function (stackObj){
+    if(stackObj.arr.length === 0) return underFlow();
     stackObj.arr.shift();
 };
 
+const overFlow = ()=>{
+    return "Stack overflow";
+};
+
+const underFlow = ()=>{
+    return "Stack Underflow";
+};
+
 Stack.prototype.peek = function (stackObj){
+    if(stackObj.arr.length === 0) return underFlow();
     return stackObj.arr[0];
 };
 
@@ -19,6 +32,12 @@ Stack.prototype.peek = function (stackObj){
 const stack = new Stack([]);
 stack.push(1,stack);
 stack.push(2,stack);
-stack.pop(stack);
-console.log('peek the element: ',stack.peek(stack));
+stack.push(3,stack);
+stack.push(4,stack);
+stack.push(5,stack);
+console.log('pushing element:',stack.push(6,stack));
+
+// stack.pop(stack);
+// stack.pop(stack);
+// console.log('peek the element: ',stack.peek(stack));
 // console.log('stack.arr after push 1: ',stack.arr);

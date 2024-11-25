a8d82d9c40be7aa7044b8aca56eb22ff8aafecd5
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-25 15:03:15 +0530

    peek,pop

--- Stack.js
+++ Stack.js
@@ -4,9 +4,21 @@ function Stack(arr){
 };
 
 Stack.prototype.push = function (element,stackObj){
-    console.log("Pushing element: ", element);
     stackObj.arr.push(element);
 };
+
+Stack.prototype.pop = function (stackObj){
+    stackObj.arr.shift();
+};
+
+Stack.prototype.peek = function (stackObj){
+    return stackObj.arr[0];
+};
+
 // [1 ,2 ,1 ,3 ,2 ,1 ,4 ,2]
 const stack = new Stack([]);
 stack.push(1,stack);
+stack.push(2,stack);
+stack.pop(stack);
+console.log('peek the element: ',stack.peek(stack));
+// console.log('stack.arr after push 1: ',stack.arr);

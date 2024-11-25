873ffba46dd7f06d848dde7372c00fd7ecafdba2
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-25 16:33:56 +0530

    randomNumber

--- randomNumber.js
+++ randomNumber.js
@@ -1,7 +1,16 @@
 function randomNumber(arr) {
-    
+    for(let i=0;i<5;i++){
+        arr.push({[i]:[]})
+    }
 
-}
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length;j++){
+            arr[i][i].push(Math.floor(Math.random() * 100));
+        }
+        // arr[i][i].push(Math.floor(Math.random() * 100));
+    }
+    console.log('arr...',arr);
 
-const arr = Array.from(5);
-const rndObj = randomNumber(arr);
+};
+
+const rndObj = randomNumber([]);

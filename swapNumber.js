6db71225ad3e7096bedd52a73777702c2721be69
tejaswiniPujari <tejaswinidinkarpujari@gmail.com>
2024-12-04 16:15:18 +0530

    swapNumber

---
+++ swapNumber.js
@@ -0,0 +1,13 @@
+function swapNumber(){
+    let x = 90;
+    let y = 17;
+    
+    x = x-y;
+    y = y+x;
+    x = y-x;
+    
+    console.log('x..',x);
+    console.log('y....',y);
+}
+
+swapNumber();
